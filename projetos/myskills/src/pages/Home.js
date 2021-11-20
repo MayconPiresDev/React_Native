ef6b4b26285c3da759e5ce4e2bc3a394d55181bc
Maycon Pires <mayconpires4@gmail.com>
2021-11-19 22:40:06 -0300

    Estilizanção de fonte

--- projetos/myskills/src/pages/Home.js
+++ projetos/myskills/src/pages/Home.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 export function Home() {
   return (
-    <View style={{
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center'
-    }}>
+    <View style={styles.container}>
       <Text>React Native</Text>
       <Text>Ignite</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
