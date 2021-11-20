867af6864de5a6d50b3f9e0176dd6f15aaff5d72
Maycon Pires <mayconpires4@gmail.com>
2021-11-19 23:39:47 -0300

    Estilização do App

--- projetos/myskills/src/pages/Home.js
+++ projetos/myskills/src/pages/Home.js
@@ -1,11 +1,37 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { 
+  View, 
+  Text, 
+  StyleSheet, 
+  TextInput, 
+  Platform,
+  TouchableOpacity
+} from 'react-native';
 
 export function Home() {
   return (
     <View style={styles.container}>
-      <Text>React Native</Text>
-      <Text>Ignite</Text>
+      <Text style={styles.title}>
+        Welcome, Maycon
+      </Text>
+
+      <TextInput 
+          style={styles.input}
+          placeholder='New skill'
+          placeholderTextColor='#444'
+      />
+
+      <TouchableOpacity 
+        style={styles.button}
+        activeOpacity={.7}
+      >
+          <Text style={styles.buttonText}>Add</Text>
+      </TouchableOpacity>
+
+      <Text style={[styles.title, { marginTop: 50 }]}>
+          My Skills
+      </Text>
+    
     </View>
   )
 }
@@ -13,7 +39,34 @@ export function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    backgroundColor: '#121015',
+    paddingHorizontal: 20,
+    paddingVertical: 70,
+    paddingHorizontal: 30
+  },
+  title: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  input: {
+    backgroundColor: '#1F1e25',
+    color: '#fff',
+    fontSize: 18,
+    padding: Platform.OS === 'ios' ? 15 : 10,
+    marginTop: 30,
+    borderRadius: 9
+  },
+  button: {
+    backgroundColor: '#A370F7',
+    padding: 15,
+    borderRadius: 7,
+    alignItems: 'center',
+    marginTop: 18
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 17,
+    fontWeight: 'bold'
   }
 })
