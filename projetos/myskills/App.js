be5d9f3e30e7043b63a78421382d0a0daa396a0b
Maycon Pires <mayconpires4@gmail.com>
2021-11-19 22:19:52 -0300

    Estruturando JSX

--- projetos/myskills/App.js
+++ projetos/myskills/App.js
@@ -59,36 +59,7 @@ const App: () => Node = () => {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+import { Home } from './src/pages/Home';
 
 const styles = StyleSheet.create({
   sectionContainer: {
@@ -109,4 +80,6 @@ const styles = StyleSheet.create({
   },
 });
 
-export default App;
+export default function App() {
+  return <Home />
+}
