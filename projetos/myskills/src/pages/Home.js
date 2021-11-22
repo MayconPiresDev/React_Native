fd4f0b4a0b70498ec3561996ca6657f11c5e5efc
Maycon Pires <mayconpires4@gmail.com>
2021-11-21 23:34:21 -0300

    Introdução a imutabilidade

--- projetos/myskills/src/pages/Home.js
+++ projetos/myskills/src/pages/Home.js
@@ -1,72 +1,105 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { 
-  View, 
-  Text, 
-  StyleSheet, 
-  TextInput, 
-  Platform,
-  TouchableOpacity
+	View, 
+	Text, 
+	StyleSheet, 
+	TextInput, 
+	Platform,
+	TouchableOpacity
 } from 'react-native';
 
 export function Home() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>
-        Welcome, Maycon
-      </Text>
+	const [newSkill, setNewSkill] = useState('');
+	const [mySkills, setMySkills] = useState([]);
 
-      <TextInput 
-          style={styles.input}
-          placeholder='New skill'
-          placeholderTextColor='#444'
-      />
+	function handleAddNewSkill() {
+      setMySkills(oldState => [...oldState, newSkill]);
 
-      <TouchableOpacity 
-        style={styles.button}
-        activeOpacity={.7}
-      >
-          <Text style={styles.buttonText}>Add</Text>
-      </TouchableOpacity>
 
-      <Text style={[styles.title, { marginTop: 50 }]}>
-          My Skills
-      </Text>
-    
-    </View>
-  )
+	}
+
+	return (
+		<View style={styles.container}>
+			<Text style={styles.title}>
+				Welcome, Maycon
+			</Text>
+
+			<TextInput 
+					style={styles.input}
+					placeholder='New skill'
+					placeholderTextColor='#444'
+					onChangeText={setNewSkill}
+			/>
+
+			<TouchableOpacity 
+				style={styles.button}
+				activeOpacity={.7}
+        onPress={handleAddNewSkill}
+			>
+					<Text style={styles.buttonText}>Add</Text>
+			</TouchableOpacity>
+
+			<Text style={[styles.title, { marginVertical: 50 }]}>
+					My skills
+			</Text>
+
+			{
+        mySkills.map(skill => (
+        <TouchableOpacity key={skill} style={styles.buttonSkill}>
+          <Text style={styles.textSkill}>
+              {skill}
+          </Text>
+        </TouchableOpacity>
+        ))
+      }
+		
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#121015',
-    paddingHorizontal: 20,
-    paddingVertical: 70,
-    paddingHorizontal: 30
-  },
-  title: {
-    color: '#fff',
-    fontSize: 24,
-    fontWeight: 'bold'
-  },
-  input: {
-    backgroundColor: '#1F1e25',
-    color: '#fff',
-    fontSize: 18,
-    padding: Platform.OS === 'ios' ? 15 : 10,
-    marginTop: 30,
-    borderRadius: 9
-  },
-  button: {
-    backgroundColor: '#A370F7',
-    padding: 15,
-    borderRadius: 7,
-    alignItems: 'center',
-    marginTop: 18
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 17,
-    fontWeight: 'bold'
-  }
+	container: {
+		flex: 1,
+		backgroundColor: '#121015',
+		paddingHorizontal: 20,
+		paddingVertical: 70,
+		paddingHorizontal: 30
+	},
+	title: {
+		color: '#fff',
+		fontSize: 24,
+		fontWeight: 'bold'
+	},
+	input: {
+		backgroundColor: '#1F1e25',
+		color: '#fff',
+		fontSize: 18,
+		padding: Platform.OS === 'ios' ? 15 : 10,
+		marginTop: 30,
+		borderRadius: 9
+	},
+	button: {
+		backgroundColor: '#A370F7',
+		padding: 15,
+		borderRadius: 7,
+		alignItems: 'center',
+		marginTop: 18
+	},
+	buttonText: {
+		color: '#FFF',
+		fontSize: 17,
+		fontWeight: 'bold'
+	},
+	buttonSkill: {
+		backgroundColor: '#1F1e25',
+		padding: Platform.OS === 'ios' ? 15 : 10,
+		borderRadius: 50,
+		alignItems: 'center',
+    marginVertical: 10
+	},
+	textSkill: {
+		color: '#FFF',
+		fontSize: 22,
+		fontWeight: 'bold',
+	}
 })
