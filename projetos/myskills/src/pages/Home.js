3545a27e8881b20225a2b6bffac574cdbee1214d
Maycon Pires <mayconpires4@gmail.com>
2021-11-30 23:00:26 -0300

    Using Hook useEffect

--- projetos/myskills/src/pages/Home.js
+++ projetos/myskills/src/pages/Home.js
@@ -1,29 +1,53 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
 	View,
 	Text,
 	StyleSheet,
 	TextInput,
 	Platform,
-	FlatList
+	FlatList,
+	StatusBar
 } from 'react-native';
 import { Button } from '../components/Button';
 import { SkillCard } from '../components/SkillCard';
 
 export function Home() {
+	// store an empty state and reload when start app in state initial
 	const [newSkill, setNewSkill] = useState('');
 	const [mySkills, setMySkills] = useState([]);
+	const [greeting, setGreeting] = useState('');
 
+
+	// Hooks
 	function handleAddNewSkill() {
+		// adds a new skill keeping the old ones creating a new vector
 		setMySkills(oldState => [...oldState, newSkill]);
-
-
 	}
+
+	useEffect(() => {
+		//effect observed when variable `[]` is changed
+		const currentHours = new Date().getHours();
+
+		if(currentHours < 12) {
+			setGreeting('Good Morning');
+		}
+		else if(currentHours >= 12 && currentHours < 18) {
+			setGreeting('Good Afternoon');
+		}
+		else {
+			setGreeting('Good Night');
+		}
+
+	}, [])
 
 	return (
 		<View style={styles.container}>
 			<Text style={styles.title}>
 				Welcome, Maycon
+			</Text>
+
+			<Text style={styles.greetings}>
+				{ greeting }
 			</Text>
 
 			<TextInput
@@ -74,5 +98,8 @@ const styles = StyleSheet.create({
 		padding: Platform.OS === 'ios' ? 15 : 10, // adapt between iOS or Android
 		marginTop: 30,
 		borderRadius: 9
+	},
+	greetings: {
+		color: '#fff',
 	}
 })
